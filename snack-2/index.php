b36cbe42195685aeb9a7ed25f8553d5cf7ea415a
fabiodelli <ntqdel@hotmail.com>
2023-05-04 12:58:55 +0200

    add snack-2

---
+++ snack-2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="get" action="verifica.php">
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="mail">Email:</label>
+    <input type="email" id="mail" name="mail"><br><br>
+    <label for="age">Età:</label>
+    <input type="text" id="age" name="age"><br><br>
+    <input type="submit" value="Accedi">
+  </form>
+</body>
+</html>
